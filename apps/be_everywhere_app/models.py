e1d4fb033ca0665f6d37efa69fd838912689def5
Noah Neumark <noahneumark@gmail.com>
2017-02-28 11:09:18 -0800

    added login/reg validation

--- apps/be_everywhere_app/models.py
+++ apps/be_everywhere_app/models.py
@@ -1,6 +1,39 @@
 from __future__ import unicode_literals
-
+import re
+import bcrypt
 from django.db import models
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+class UserManager(models.Manager):
+    def regvalidation(self, postData):
+        errors = []
+        if len(postData['first']) < 2:
+            errors.append("First name must be at least 2 character.")
+        if (postData['first'].replace(' ','').isalpha() == False):
+            errors.append("First name must only be letters.")
+        if len(postData['last']) < 2:
+            errors.append("Last name must be at least 2 character.")
+        if (postData['last'].replace(' ','').isalpha() == False):
+            errors.append("Last name must only be letters.")
+        if not EMAIL_REGEX.match(postData['email']):
+            errors.append("Not a valid email.")
+        if Users.objects.filter(email=postData['email']):
+            errors.append("Email already taken.")
+        if len(postData['password']) < 8:
+            errors.append("Password must be at least 8 characters.")
+        elif postData['password'] != postData['confpass']:
+            errors.append("Password does not match password confirmation.")
+        return errors
+    def loginvalidation(self, postData):
+        errors = []
+        loginuser = Users.objects.filter(email=postData['email'])
+        if loginuser:
+            passtest = loginuser[0].password.encode()
+        if not loginuser or bcrypt.hashpw(postData['password'].encode(), passtest) != passtest:
+            errors.append("Invalid login")
+        return errors
+
 
 class Users(models.Model):
     first = models.CharField(max_length = 100)
