587725c017bcd0ab32ae9c3602bc5d3053c2a509
Mitch <mitchellw.andrews@gmail.com>
2017-02-28 11:12:48 -0800

    message'

--- apps/be_everywhere_app/forms.py
+++ apps/be_everywhere_app/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from .models import Users
 
 class RegisterForm(forms.ModelForm):
+    pass_conf = forms.CharField(max_length = 100, widgets=forms.PasswordInput)
     class Meta:
         model = Users
-        fields = ['first', 'last', 'email', 'password']
+        fields = ['first', 'last', 'email', 'password'] + ['pass_conf']
         widgets = {'password':forms.PasswordInput()}
