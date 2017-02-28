736488995df51f7b83dd846a94c98a6655a1f691
Mitch <mitchellw.andrews@gmail.com>
2017-02-27 17:10:33 -0800

    changes

--- apps/be_everywhere_app/urls.py
+++ apps/be_everywhere_app/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.homepage)
-	url(r'^log_reg$', views.log_reg)
+	url(r'^$', views.homepage),
+	url(r'^log_reg$', views.log_reg),
 ]
