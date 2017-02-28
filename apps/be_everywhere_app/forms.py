271a005957dd14fd947b0e0ed9c9e9ab6371e61a
Mitch <mitchellw.andrews@gmail.com>
2017-02-28 12:33:24 -0800

    name change

--- apps/be_everywhere_app/forms.py
+++ apps/be_everywhere_app/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from .models import Users
 
 class RegisterForm(forms.ModelForm):
-    confirm_assword = forms.CharField(max_length = 100, widget=forms.PasswordInput())
+    confirm_password = forms.CharField(max_length = 100, widget=forms.PasswordInput())
     class Meta:
         model = Users
-        fields = ['first', 'last', 'email', 'password'] + ['confirm_assword']
+        fields = ['first', 'last', 'email', 'password'] + ['confirm_password']
         widgets = {'password':forms.PasswordInput(),}
 
 class LoginForm(forms.ModelForm):
