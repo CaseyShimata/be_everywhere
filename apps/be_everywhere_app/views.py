bb24ab6e5467ef0ade68c1f651f73955dc12f448
Casey Shimata <shimatacb@Caseys-MacBook-Pro.local>
2017-02-28 12:02:16 -0800

    new commits

--- apps/be_everywhere_app/views.py
+++ apps/be_everywhere_app/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 
 # Create your views here.
 def homepage(request):
     return render(request, 'be_everywhere_app/homepage.html')
 
 def log_reg(request):
-    form = RegisterForm()
-    context = {'regform':form}
+    regform = RegisterForm()
+    logform = LoginForm()
+    context = {'regform':regform, 'logform':logform}
     return render(request, 'be_everywhere_app/login_registration.html', context)
 
 def about(request):
