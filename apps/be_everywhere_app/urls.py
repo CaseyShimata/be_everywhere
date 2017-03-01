c2f8914fda03d24f3d6bd3156aad1d17cd04ebe9
shimatacb <shimatacb@gmail.com>
2017-02-28 17:17:33 -0800

    updated a bit during wireframe info

--- apps/be_everywhere_app/urls.py
+++ apps/be_everywhere_app/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
 	url(r'^log_reg$', views.log_reg),
 	url(r'^about$', views.about),
 	url(r'^register$', views.register),
-<<<<<<< HEAD
 	url(r'^admin_manage_users$', views.admin_manage_users),
 	url(r'^user_manage_events$', views.user_manage_events),
 	url(r'^admin_manage_products$', views.admin_manage_products),
@@ -17,17 +16,5 @@ urlpatterns = [
 	url(r'^view_user$', views.view_user),
 	url(r'^welcome_admin$', views.welcome_admin),
 	url(r'^welcome_attendee$', views.welcome_attendee),
-=======
 	url(r'^login$', views.login),
-	# url(r'^admin_manage_users$', views.admin_manage_users),
-	# url(r'^user_manage_events$', views.user_manage_events),
-	# url(r'^admin_manage_products$', views.admin_manage_products),
-	# url(r'^admin_product_sales$', views.admin_product_sales),
-	# url(r'^attendees_become_an_operator$', views.attendees_become_an_operator),
-	# url(r'^my_messages$', views.my_messages),
-	# url(r'^view_event$', views.view_event),
-	# url(r'^view_user$', views.view_user),
-	# url(r'^welcome_admin$', views.welcome_admin),
-	# url(r'^welcome_attendee$', views.welcome_attendee),
->>>>>>> fa351ae0b6d80d31b8ce4d651bb4234e0f8dfdf4
 ]
