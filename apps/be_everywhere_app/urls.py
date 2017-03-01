83bdf0d657246e89dade472c8de369d030b612e7
shimatacb <shimatacb@gmail.com>
2017-03-01 00:15:40 -0800

    git hub pulling down fucked up my file so I grabbed my hard copy back up of the updated urls.py

--- apps/be_everywhere_app/urls.py
+++ apps/be_everywhere_app/urls.py
@@ -23,23 +23,5 @@ urlpatterns = [
 #  all above url's are page render routes and urls
 
 	url(r'^login$', views.login),
-<<<<<<< HEAD
 	url(r'^register$', views.register),
-=======
-<<<<<<< HEAD
-=======
-	url(r'^admin_manage_events$', views.admin_manage_events),
-	# url(r'^admin_manage_users$', views.admin_manage_users),
-	url(r'^user_manage_events$', views.user_manage_events),
-	# url(r'^admin_manage_products$', views.admin_manage_products),
-	# url(r'^admin_product_sales$', views.admin_product_sales),
-	# url(r'^attendees_become_an_operator$', views.attendees_become_an_operator),
-	# url(r'^my_messages$', views.my_messages),
-	# url(r'^view_event$', views.view_event),
-	# url(r'^view_user$', views.view_user),
-	url(r'^welcome_admin$', views.welcome_admin),
-	# url(r'^welcome_attendee$', views.welcome_attendee),
-	# url(r'^create_event$'. views.create_event),
->>>>>>> 50c4834b05f7390ae149a48ad4484e4fbe71deb1
->>>>>>> a2f413581dc0f771eff440e005bef4a394aedc0b
 ]
--- apps/be_everywhere_app/views.py
+++ apps/be_everywhere_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.urlresolvers import reverse
-from .forms import RegisterForm, LoginForm, EventForm
-from models import Users, Genres
+from .forms import RegisterForm, LoginForm
+from models import Users
 
 # Create your views here.
 def homepage(request):
@@ -50,10 +50,7 @@ def login(request):
                'last': user['theuser'].last,
                'permission': user['theuser'].permission
            }
-        if request.session['logged']['permission'] == 1:
-            return redirect('/welcome_admin')
-        else:
-            return redirect('/')
+        return redirect('/')
 #alex code
 def admin_manage_users(request):
     if ['logged'].permission == 3: #assuming basic users are 1, admin are 3 and vendors 1?
@@ -84,39 +81,14 @@ def admin_manage_events(request):
 def admin_manage_my_account(request):
     return render(request, 'be_everywhere_app/admin_manage_my_account.html')
 
-def admin_manage_events(request):
-    eventform = EventForm()
-    context = {'eventform':eventform,
-    'genre':Genres.objects.all()}
-    return render(request, 'be_everywhere_app/admin_manage_events.html', context)
-
 def admin_manage_products(request):
     return render(request, 'be_everywhere_app/admin_manage_products.html')
-
-def admin_manage_events(request):
-    if ['logged'].permission == 3: #assuming basic users are 1, admin are 3 and vendors 1?
-        context = {
-            'all_events': Events.objects.all()
-        }
-        return render(request, 'be_everywhere_app/welcome_admin.html')
-    else
-        return redirect ('/homepage')
 
 def admin_product_sales(request):
     return render(request, 'be_everywhere_app/admin_product_sales.html')
 
-<<<<<<< HEAD
 def attendees_manage_my_account(request):
     return render(request, 'be_everywhere_app/attendees_manage_my_account.html')
-=======
-def attendee_manage_events(request):
-    context = {
-        ['logged'].id: id,
-        'my_events': Events.objects.filter(=id),
-        'all_events': Events.objects.all()
-    }
-    return render(request, 'be_everywhere_app/attendee_manage_events.html', context)
->>>>>>> a2f413581dc0f771eff440e005bef4a394aedc0b
 
 def attendees_become_an_operator(request):
     return render(request, 'be_everywhere_app/attendees_become_an_operator.html')
@@ -140,20 +112,7 @@ def view_user(request):
     return render(request, 'be_everywhere_app/view_user.html')
 
 def welcome_admin(request):
-    if ['logged'].permission == 3: #assuming basic users are 1, admin are 3 and vendors 1?
-        return render(request, 'be_everywhere_app/welcome_admin.html')
-    else
-        return redirect ('/homepage')
+    return render(request, 'be_everywhere_app/welcome_admin.html')
 
 def welcome_attendee(request):
-<<<<<<< HEAD
     return render(request, 'be_everywhere_app/welcome_attendee.html')
-
-# def create_event(request):
-    # Events.objects.new(request.POST)
-=======
-    if ['logged'].permission == 1:
-        return render(request, 'be_everywhere_app/welcome_attendee.html')
-    else
-        return redirect ('/homepage')
->>>>>>> b6c644b2d72204ff91b82c5eba202b1914d8c9ff
